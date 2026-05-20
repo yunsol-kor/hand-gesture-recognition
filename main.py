5da8d7793f0b6154b909ed15e54f94f3a1501d66
yunsol-kor <chw031125@ajou.ac.kr>
2026-05-20 15:57:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import cv2
 import time
 from cvzone.HandTrackingModule import HandDetector
 
-# 손 감지기 생성
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
-# 웹캠 시작
 cap = cv2.VideoCapture(0)
 
 capture_count = 0
@@ -14,17 +12,15 @@ while True:
 
     success, img = cap.read()
 
-    # 손 감지
+  
     hands, img = detector.findHands(img)
 
     if hands:
 
         hand = hands[0]
 
-        # 손가락 상태 확인
         fingers = detector.fingersUp(hand)
 
-        # ✌️ 제스처 감지
         if fingers == [0,1,1,0,0]:
 
             filename = f'capture_{capture_count}.jpg'
@@ -45,7 +41,7 @@ while True:
             capture_count += 1
             time.sleep(1)
 
-    # 화면 출력
+
     cv2.imshow("Hand Gesture Capture", img)
 
     # 종료
